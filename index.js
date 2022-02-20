069c6e8b4c3b60828c8c9f15e1eebb88d8cb7fe9
mr-cat-sudo <71202514+mr-cat-sudo@users.noreply.github.com>
2022-02-20 13:24:33 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//dont change this unless you know what your doing!!!
+
 const keepAlive = require("./server")
    
 const axios = require("axios")
-const urls = ["https://drago-working-automod-bot.clashwith2.repl.co"]
+const urls = ["YOUR SERVER OR SOMETHING"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
